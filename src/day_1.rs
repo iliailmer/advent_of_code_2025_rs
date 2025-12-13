aeb11acc1aed593665e3d73fcc61d78f551ac849
Ilia Ilmer <contact@iliailmer.com>
2025-12-13 17:14:06 -0500

    day 1

---
+++ src/day_1.rs
@@ -0,0 +1,78 @@
+use std::{
+    fs::File,
+    io::{Error, Read},
+    ops::Rem,
+};
+pub fn read_input_file(file_path: String) -> Result<String, Error> {
+    // read a file
+    let mut file: File = File::open(file_path)?;
+    let mut puzzle: String = String::new();
+    file.read_to_string(&mut puzzle)?;
+    Ok(puzzle)
+}
+pub fn part_1(input: &str) -> i32 {
+    let mut result = 0;
+    let mut dial = 50;
+    for line in input.lines() {
+        let direction = line
+            .chars()
+            .next()
+            .map(|c| match c {
+                'L' => -1,
+                'R' => 1,
+                _ => 0,
+            })
+            .unwrap();
+        let step: i32 = line.get(1..).unwrap().parse().unwrap();
+        dial += direction * step;
+        dial %= 100;
+
+        if dial == 0 {
+            result += 1;
+        }
+    }
+    result
+}
+
+pub fn part_2(input: &str) -> i32 {
+    let mut result = 0;
+    let mut dial = 50;
+    for line in input.lines() {
+        let direction: i32 = line
+            .chars()
+            .next()
+            .map(|c| match c {
+                'L' => -1,
+                'R' => 1,
+                _ => 0,
+            })
+            .unwrap();
+        let step: i32 = line.get(1..).unwrap().parse().unwrap();
+        let quot: i32 = direction.div_euclid(100);
+        let rem = direction.rem(100);
+
+        if direction < 0 {
+            result += quot;
+            if dial > 0 && dial - rem < 0 {
+                result += 1;
+            }
+            dial += direction * step;
+        }
+        if direction > 0 {
+            if dial < 100 && dial + step > 100 {
+                result += quot;
+            }
+            if dial < 100 && dial + rem > 100 {
+                result += 1;
+            }
+            dial += direction * step;
+        }
+
+        dial %= 100;
+
+        if dial == 0 {
+            result += 1;
+        }
+    }
+    result
+}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod day_1;
+mod day_2;
+fn main() {
+    // let input = day_1::read_input_file("inputs/day_1/puzzle_1.txt".to_string()).unwrap();
+    // let result_1 = day_1::part_1(&input);
+    // assert_eq!(result_1, 1029);
+    // let result_2 = day_1::part_2(&input);
+    // assert_eq!(result_2, 5892);
+    let day2_result: i64 = day_2::day_2();
+    assert_eq!(day2_result, 29818212493);
+}
