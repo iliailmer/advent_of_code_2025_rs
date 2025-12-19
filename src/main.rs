8ca277497af6110c5a5b1bc2d924bd75ba39463f
Ilia Ilmer <contact@iliailmer.com>
2025-12-18 22:35:43 -0500

    add cli

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
+use clap::Parser;
+
 #[allow(dead_code)]
 mod day_1;
 mod day_2;
+mod day_3;
+
+/// Advent of Code CLI
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// Day number (e.g. 1, 2, 3)
+    #[arg(long)]
+    day: u8,
+}
+
 fn main() {
-    // let input = day_1::read_input_file("inputs/day_1/puzzle_1.txt".to_string()).unwrap();
-    // let result_1 = day_1::part_1(&input);
-    // assert_eq!(result_1, 1029);
-    // let result_2 = day_1::part_2(&input);
-    // assert_eq!(result_2, 5892);
-    let day2_result: i64 = day_2::day_2_pt_1();
-    assert_eq!(day2_result, 29818212493);
-    let day2_result: i64 = day_2::day_2_pt_2();
-    assert_eq!(day2_result, 37432260594);
+    let args = Args::parse();
+
+    match args.day {
+        1 => {
+            let day2_result_1: i64 = day_2::day_2_pt_1();
+            let day2_result_2: i64 = day_2::day_2_pt_2();
+            println!("Day 2, Part 1: {day2_result_1}");
+            println!("Day 2, Part 2: {day2_result_2}");
+        }
+        2 => {
+            let day2_result_1: i64 = day_2::day_2_pt_1();
+            let day2_result_2: i64 = day_2::day_2_pt_2();
+            println!("Day 2, Part 1: {day2_result_1}");
+            println!("Day 2, Part 2: {day2_result_2}");
+        }
+        // Add more cases for other days/parts as needed
+        _ => {
+            eprintln!("Day {} not implemented.", args.day);
+        }
+    }
 }
